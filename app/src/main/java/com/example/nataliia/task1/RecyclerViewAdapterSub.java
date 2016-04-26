8838bba308c165d88b6f13da7a156ae6972d0cb1
deshkaa <natalyadesh9@gmail.com>
2016-04-26 13:24:50 +0300

    Data to model from 1st task added

---
+++ app/src/main/java/com/example/nataliia/task1/RecyclerViewAdapterSub.java
@@ -0,0 +1,48 @@
+package com.example.nataliia.task1;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.nataliia.task2_deshkonataliia.R;
+import com.squareup.picasso.Picasso;
+
+public class RecyclerViewAdapterSub extends RecyclerView.Adapter<com.example.nataliia.task1.RecyclerViewAdapterSub.ViewHolder> {
+
+    private int[] mDataset;
+    private View.OnClickListener onClickListener;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mImageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mImageView = (ImageView) v.findViewById(R.id.imageView);
+        }
+    }
+
+    public RecyclerViewAdapterSub(int[] dataset, View.OnClickListener listener) {
+        mDataset = dataset;
+        this.onClickListener = listener;
+    }
+
+    @Override
+    public com.example.nataliia.task1.RecyclerViewAdapterSub.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.image_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Picasso.with(holder.mImageView.getContext()).load(mDataset[position]).into(holder.mImageView);
+        holder.mImageView.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
